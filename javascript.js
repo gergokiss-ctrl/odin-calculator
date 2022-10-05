6442e3e7a96776deceeac1a02d3af0e2049cdb15
gergokiss-ctrl <kiss.gergo55@gmail.com>
2022-10-05 18:31:53 +0200

    Create operate() function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,31 @@
+let firstNumber = null;
+let secondNumber = null;
+let operator = null;
+let result = 0;
+
+// console.log('test1');
+// console.log(firstNumber);
+// console.log(secondNumber);
+// console.log(operator);
+// console.log(result);
+
+// firstNumber = 420;
+// secondNumber = 69;
+// operator = '-';
+
+// console.log('test2');
+// console.log(firstNumber);
+// console.log(secondNumber);
+// console.log(operator);
+// console.log(result);
+
+// operate(operator, firstNumber, secondNumber);
+// console.log('test3');
+// console.log(firstNumber);
+// console.log(secondNumber);
+// console.log(operator);
+// console.log(result);
+
 function add(firstNumber, secondNumber) {
     return firstNumber + secondNumber;
 }
@@ -12,4 +40,24 @@ function multiply(firstNumber, secondNumber) {
 
 function divide(firstNumber, secondNumber) {
     return firstNumber / secondNumber;
+}
+
+function operate(operator, firstNumber, secondNumber) {
+    if ((firstNumber && secondNumber && operator) != null) {
+        if (operator == '+') {
+            result = add(firstNumber, secondNumber);
+        }
+
+        if (operator == '-') {
+            result = subtract(firstNumber, secondNumber);
+        }
+
+        if (operator == '*') {
+            result = multiply(firstNumber, secondNumber);
+        }
+
+        if (operator == '/') {
+            result = divide(firstNumber, secondNumber);
+        }
+    }
 }
